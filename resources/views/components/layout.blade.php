2048c5b16a00e9072b0a44a2b6b84b827e3c1ef7
oheneadj <oheneadjei.dev@gmail.com>
2023-04-18 17:20:26 +0000

    added logo

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -61,7 +61,7 @@
                 </button>
                 <h1 class="navbar-brand navbar-brand-autodark d-none-navbar-horizontal pe-0 pe-md-3">
                     <a href="/">
-                        <img src="#" width="110" height="32" alt="64Devs" class="navbar-brand-image">
+                        <img src="/64devs-logo.png" width="100" height="30" alt="64Devs" class="navbar-brand-image rounded">
                     </a>
                 </h1>
                 <div class="navbar-nav flex-row order-md-last">
